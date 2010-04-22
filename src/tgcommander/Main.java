e190f576bbc8ce311093ed824e1b70486ab77f2e
kadari <ktorpi@ktorpi-desktop.(none)>
2010-04-22 16:56:34 +0200

    Apróbb módosítások...

--- src/tgcommander/Main.java
+++ src/tgcommander/Main.java
@@ -1,14 +1,18 @@
 package tgcommander;
 
 import java.io.File;
+import javax.swing.text.DefaultEditorKit.CopyAction;
 /**
  * Futattható osztály.
  * @author ktorpi
  */
 public class Main {
     public static void main(String[] args) {
-        File file = new File(args[0]);
+        File file1 = new File(args[0]);
+        File file2 = new File(args[1]);
+
         Operation muv = new Operation();
-        muv.listDir(file);
+        // muv.copyFile(file1, file2);
+        muv.copyFile(args[0], args[1]);
     }
 }
--- src/tgcommander/Operation.java
+++ src/tgcommander/Operation.java
@@ -21,8 +21,9 @@ public class Operation {
 
     /**
      * Fájl másolása.
+     * FIXME: még majd kezelni kell a felülírást, meg az arra rákérdezést
      * @param source A forrásfájl elérési útvonala.
-     * @param dest A célfájl elérési útvonala. Egyenlőre fájl!
+     * @param dest A célkönyvtár elérési útvonala.
      */
     void copyFile(String source, String dest) {
         int b;
@@ -31,12 +32,11 @@ public class Operation {
 
         try {
             in = new FileInputStream(source);
-            out = new FileOutputStream(dest);
+            out = new FileOutputStream(dest + source.substring(source.lastIndexOf("/")));
             while ((b = in.read()) != -1) {             // olvasás-írás bájtonként
                 out.write(b);
             }
         } catch (IOException e){
-            // FIXME: lehet hogy nem a System.err-re kéne irni ha már desktop application
             System.err.println(e.getMessage());
         } finally {                                     // állományok lezárása
             try {
@@ -47,7 +47,6 @@ public class Operation {
                     out.close();
                 }
             } catch (IOException e) {
-                // FIXME:
                 System.err.println(e.getMessage());
             }
         }
@@ -55,8 +54,9 @@ public class Operation {
 
     /**
      * Fájl másolása.
+     * FIXME: még majd kezelni kell a felülírást, meg az arra rákérdezést
      * @param source A forrsáfájl bejegyzés.
-     * @param dest Célfájl bejegyzés. Egyenlőre fájl!
+     * @param dest Célkönyvtár bejegyzés.
      */
     void copyFile(File source, File dest) {
         int b;
@@ -65,12 +65,11 @@ public class Operation {
 
         try {
             in = new FileInputStream(source);
-            out = new FileOutputStream(dest);
+            out = new FileOutputStream(dest.getPath() + "/" + source.getName());
             while ((b = in.read()) != -1) {             // olvasás-írás bájtonként
                 out.write(b);
             }
         } catch (IOException e){
-            // FIXME: lehet hogy nem a System.err-re kéne irni ha már desktop application
             System.err.println(e.getMessage());
         } finally {                                     // állományok lezárása
             try {
@@ -81,7 +80,6 @@ public class Operation {
                     out.close();
                 }
             } catch (IOException e) {
-                // FIXME:
                 System.err.println(e.getMessage());
             }
         }
@@ -93,29 +91,32 @@ public class Operation {
      */
     void listDir(File dir) {
         File[] files = dir.listFiles();
-        String name, ext, size, date, attr;
+        String name,                                    // név
+               ext,                                     // kiterjesztés ha nem mappa
+               size,                                    // méret (egyenlőre bájtokban)
+               date,                                    // utolsó módosítás dátuma
+               attr;                                    // olvasható-írható attribútumok
 
         // először a könyvtárak
-
         for (File i : files) {
             if (i.isDirectory()) {
                 DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
-                date = df.format(i.lastModified());
+                date = df.format(i.lastModified());     // utolsó módosítás dátumának formázása
                 attr = (i.canRead()) ? "r" : "-";
                 attr += (i.canWrite()) ? "w" : "-";
-
                 name = i.getName();
+                // könyvtár esetén nincs kiterjesztés, a méret: "<DIR>"
                 ext = "";
                 size = "<DIR>";
                 System.out.println(name + "\t" + ext + "\t" + size + "\t" + date + "\t" + attr);
             }
         }
 
-        // majd a fájlok
+        // majd a fájlok listázása
         for (File i : files) {
             if (i.isFile()) {
                 DateFormat df = DateFormat.getDateTimeInstance(DateFormat.MEDIUM, DateFormat.SHORT);
-                date = df.format(i.lastModified());
+                date = df.format(i.lastModified());     // utolsó módosítás dátumának formázása
                 attr = (i.canRead()) ? "r" : "-";
                 attr += (i.canWrite()) ? "w" : "-";
                 int index = i.getName().lastIndexOf(".");
