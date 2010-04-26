a4339ac61df7ca18379be9ac82b67d40897e8bc1
timo1991 <h947730@stud.u-szeged.hu>
2010-04-26 21:56:39 +0200

    GUI alapja

--- src/tgcommander/TGcommanderView.java
+++ src/tgcommander/TGcommanderView.java
@@ -101,6 +101,15 @@ public class TGcommanderView extends FrameView {
     private void initComponents() {
 
         mainPanel = new javax.swing.JPanel();
+        eszkoztar = new javax.swing.JToolBar();
+        aktualisKonyvtar = new javax.swing.JSplitPane();
+        konyvtarJobb = new javax.swing.JLabel();
+        konyvtarBal = new javax.swing.JLabel();
+        panelek = new javax.swing.JSplitPane();
+        panelBal = new javax.swing.JScrollPane();
+        listaBal = new javax.swing.JTable();
+        panelJobb = new javax.swing.JScrollPane();
+        listaJobb = new javax.swing.JTable();
         menuBar = new javax.swing.JMenuBar();
         javax.swing.JMenu fileMenu = new javax.swing.JMenu();
         javax.swing.JMenuItem exitMenuItem = new javax.swing.JMenuItem();
@@ -113,21 +122,128 @@ public class TGcommanderView extends FrameView {
         progressBar = new javax.swing.JProgressBar();
 
         mainPanel.setName("mainPanel"); // NOI18N
+        mainPanel.setPreferredSize(new java.awt.Dimension(800, 600));
+
+        eszkoztar.setRollover(true);
+        eszkoztar.setName("eszkoztar"); // NOI18N
+        eszkoztar.setPreferredSize(new java.awt.Dimension(600, 25));
+
+        aktualisKonyvtar.setDividerLocation(300);
+        aktualisKonyvtar.setDividerSize(0);
+        aktualisKonyvtar.setResizeWeight(0.5);
+        aktualisKonyvtar.setMinimumSize(new java.awt.Dimension(100, 25));
+        aktualisKonyvtar.setName("aktualisKonyvtar"); // NOI18N
+        aktualisKonyvtar.setPreferredSize(new java.awt.Dimension(100, 25));
+
+        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(tgcommander.TGcommanderApp.class).getContext().getResourceMap(TGcommanderView.class);
+        konyvtarJobb.setText(resourceMap.getString("konyvtarJobb.text")); // NOI18N
+        konyvtarJobb.setHorizontalTextPosition(javax.swing.SwingConstants.LEADING);
+        konyvtarJobb.setName("konyvtarJobb"); // NOI18N
+        konyvtarJobb.setNextFocusableComponent(listaJobb);
+        aktualisKonyvtar.setRightComponent(konyvtarJobb);
+
+        konyvtarBal.setText(resourceMap.getString("konyvtarBal.text")); // NOI18N
+        konyvtarBal.setName("konyvtarBal"); // NOI18N
+        aktualisKonyvtar.setLeftComponent(konyvtarBal);
+
+        panelek.setDividerLocation(300);
+        panelek.setResizeWeight(0.5);
+        panelek.setName("panelek"); // NOI18N
+        panelek.setPreferredSize(new java.awt.Dimension(800, 400));
+        panelek.addPropertyChangeListener(new java.beans.PropertyChangeListener() {
+            public void propertyChange(java.beans.PropertyChangeEvent evt) {
+                panelekPropertyChange(evt);
+            }
+        });
+
+        panelBal.setName("panelBal"); // NOI18N
+
+        listaBal.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Név", "Méret", "Utoljára módosítva", "Jogosultságok"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                true, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        listaBal.setName("listaBal"); // NOI18N
+        listaBal.setPreferredSize(new java.awt.Dimension(300, 400));
+        listaBal.setShowVerticalLines(false);
+        listaBal.getTableHeader().setReorderingAllowed(false);
+        listaBal.setVerifyInputWhenFocusTarget(false);
+        panelBal.setViewportView(listaBal);
+        listaBal.getColumnModel().getColumn(0).setHeaderValue(resourceMap.getString("listaBal.columnModel.title0")); // NOI18N
+        listaBal.getColumnModel().getColumn(1).setHeaderValue(resourceMap.getString("listaBal.columnModel.title1")); // NOI18N
+        listaBal.getColumnModel().getColumn(2).setHeaderValue(resourceMap.getString("listaBal.columnModel.title2")); // NOI18N
+        listaBal.getColumnModel().getColumn(3).setHeaderValue(resourceMap.getString("listaBal.columnModel.title3")); // NOI18N
+
+        panelek.setRightComponent(panelBal);
+
+        panelJobb.setName("panelJobb"); // NOI18N
+
+        listaJobb.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {"", null, null, null},
+                {null, null, null, null},
+                {null, null, null, null},
+                {null, null, null, null}
+            },
+            new String [] {
+                "Név", "Méret", "Utoljára módosítva", "Jogosultságok"
+            }
+        ) {
+            boolean[] canEdit = new boolean [] {
+                true, false, false, false
+            };
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
+        listaJobb.setName("listaJobb"); // NOI18N
+        listaJobb.setPreferredSize(new java.awt.Dimension(300, 400));
+        listaJobb.setShowVerticalLines(false);
+        listaJobb.setVerifyInputWhenFocusTarget(false);
+        panelJobb.setViewportView(listaJobb);
+        listaJobb.getColumnModel().getColumn(0).setHeaderValue(resourceMap.getString("listaJobb.columnModel.title0")); // NOI18N
+        listaJobb.getColumnModel().getColumn(1).setHeaderValue(resourceMap.getString("listaJobb.columnModel.title1")); // NOI18N
+        listaJobb.getColumnModel().getColumn(2).setHeaderValue(resourceMap.getString("listaJobb.columnModel.title2")); // NOI18N
+        listaJobb.getColumnModel().getColumn(3).setHeaderValue(resourceMap.getString("listaJobb.columnModel.title3")); // NOI18N
+
+        panelek.setLeftComponent(panelJobb);
 
         javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
         mainPanel.setLayout(mainPanelLayout);
         mainPanelLayout.setHorizontalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
+            .addComponent(eszkoztar, javax.swing.GroupLayout.DEFAULT_SIZE, 705, Short.MAX_VALUE)
+            .addComponent(aktualisKonyvtar, javax.swing.GroupLayout.DEFAULT_SIZE, 705, Short.MAX_VALUE)
+            .addComponent(panelek, javax.swing.GroupLayout.DEFAULT_SIZE, 705, Short.MAX_VALUE)
         );
         mainPanelLayout.setVerticalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 252, Short.MAX_VALUE)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addComponent(eszkoztar, javax.swing.GroupLayout.PREFERRED_SIZE, 32, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(aktualisKonyvtar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(panelek, javax.swing.GroupLayout.DEFAULT_SIZE, 333, Short.MAX_VALUE))
         );
 
         menuBar.setName("menuBar"); // NOI18N
+        menuBar.setPreferredSize(new java.awt.Dimension(600, 20));
 
-        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(tgcommander.TGcommanderApp.class).getContext().getResourceMap(TGcommanderView.class);
         fileMenu.setText(resourceMap.getString("fileMenu.text")); // NOI18N
         fileMenu.setName("fileMenu"); // NOI18N
 
@@ -148,6 +264,7 @@ public class TGcommanderView extends FrameView {
         menuBar.add(helpMenu);
 
         statusPanel.setName("statusPanel"); // NOI18N
+        statusPanel.setPreferredSize(new java.awt.Dimension(600, 25));
 
         statusPanelSeparator.setName("statusPanelSeparator"); // NOI18N
 
@@ -162,11 +279,11 @@ public class TGcommanderView extends FrameView {
         statusPanel.setLayout(statusPanelLayout);
         statusPanelLayout.setHorizontalGroup(
             statusPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(statusPanelSeparator, javax.swing.GroupLayout.DEFAULT_SIZE, 400, Short.MAX_VALUE)
+            .addComponent(statusPanelSeparator, javax.swing.GroupLayout.DEFAULT_SIZE, 705, Short.MAX_VALUE)
             .addGroup(statusPanelLayout.createSequentialGroup()
                 .addContainerGap()
                 .addComponent(statusMessageLabel)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 226, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 535, Short.MAX_VALUE)
                 .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(statusAnimationLabel)
@@ -189,9 +306,26 @@ public class TGcommanderView extends FrameView {
         setStatusBar(statusPanel);
     }// </editor-fold>//GEN-END:initComponents
 
+    private void panelekPropertyChange(java.beans.PropertyChangeEvent evt) {//GEN-FIRST:event_panelekPropertyChange
+        aktualisKonyvtar.setDividerLocation(panelek.getDividerLocation());
+    }//GEN-LAST:event_panelekPropertyChange
+
+   private void konyvtarListazas(){
+
+   }
+
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JSplitPane aktualisKonyvtar;
+    private javax.swing.JToolBar eszkoztar;
+    private javax.swing.JLabel konyvtarBal;
+    private javax.swing.JLabel konyvtarJobb;
+    private javax.swing.JTable listaBal;
+    private javax.swing.JTable listaJobb;
     private javax.swing.JPanel mainPanel;
     private javax.swing.JMenuBar menuBar;
+    private javax.swing.JScrollPane panelBal;
+    private javax.swing.JScrollPane panelJobb;
+    private javax.swing.JSplitPane panelek;
     private javax.swing.JProgressBar progressBar;
     private javax.swing.JLabel statusAnimationLabel;
     private javax.swing.JLabel statusMessageLabel;
