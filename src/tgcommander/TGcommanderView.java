69b60e76a225044bd74a8f16735a85894422c1f7
Mr.Zealot <zealot@zealot-laptop.(none)>
2010-05-06 22:24:19 +0200

    masolas, athelyezes, ujmappa, torles, rendes tab

--- src/tgcommander/TGcommanderView.java
+++ src/tgcommander/TGcommanderView.java
@@ -20,20 +20,31 @@ import java.io.*;
  */
 public class TGcommanderView extends FrameView implements MouseListener {
 
+    //az oldal tartalma
     private EFile bal;
     private EFile jobb;
+
+    //melyik oldalon van a fókusz
     private boolean focus = false;
+
+    //jelenjenek-e meg a rejtett file-ok
     private boolean balHidden = false;
     private boolean jobbHidden = false;
 
+    //hol volt
+    private int holVoltBal;
+    private int holVoltJobb;
+
     public void mouseClicked(MouseEvent e){
 
+        JTable target = (JTable)e.getSource();
+        int selection = target.getSelectedRow();
+        if ((target == listaBal && focus) || (target == listaJobb && !focus)) {
+            new ChangeFocusAction().actionPerformed(null);
+            target.getSelectionModel().setSelectionInterval(selection, selection);
+        }
+
         if (e.getClickCount() == 2) {
-            //melyik oldalra kattintottak
-            JTable target = (JTable)e.getSource();
-            if ((target == listaBal && focus) || (target == listaJobb && !focus)) {
-                new ChangeFocusAction().actionPerformed(null);
-            }
             listazasAction();
         }
     }
@@ -79,11 +90,14 @@ public class TGcommanderView extends FrameView implements MouseListener {
     public void listDir(boolean hova, EFile mit, boolean hidden) {
         EntryAttributes[] ea = mit.getContent(hidden);
         JTable target;
+
         if (hova) {
             target = listaJobb;
+            holVoltJobb = 0;
             jobb = mit;
         } else {
             target = listaBal;
+            holVoltBal = 0;
             bal = mit;
         }
         DefaultTableModel dtm = (DefaultTableModel)target.getModel();
@@ -115,14 +129,21 @@ public class TGcommanderView extends FrameView implements MouseListener {
                 return canEdit [columnIndex];
             }
         });
+        if (hova == focus) {
+            target.getSelectionModel().setSelectionInterval(0, 0);
+        }
     }
 
     public void refresh() {
         JOptionPane.showMessageDialog(menuBar, "REFRESSS");
+        int id = listaBal.getSelectedRow();
         bal = new EFile(bal.getFile());
         listDir(false,bal,balHidden);
+        listaBal.getSelectionModel().setSelectionInterval(id, id);
+        id = listaJobb.getSelectedRow();
         jobb = new EFile(jobb.getFile());
         listDir(true,jobb,jobbHidden);
+        listaJobb.getSelectionModel().setSelectionInterval(id, id);
     }
 
     public void mousePressed(MouseEvent e){}
@@ -141,10 +162,14 @@ public class TGcommanderView extends FrameView implements MouseListener {
         public void actionPerformed(ActionEvent e) {
             if (focus) {
                 listaBal.requestFocus();
+                listaBal.getSelectionModel().setSelectionInterval(holVoltBal, holVoltBal);
+                holVoltJobb = listaJobb.getSelectedRow();
                 listaJobb.clearSelection();
                 focus = false;
             } else {
                 listaJobb.requestFocus();
+                listaJobb.getSelectionModel().setSelectionInterval(holVoltJobb, holVoltJobb);
+                holVoltBal = listaBal.getSelectedRow();
                 listaBal.clearSelection();
                 focus = true;
             }
@@ -734,9 +759,10 @@ public class TGcommanderView extends FrameView implements MouseListener {
             nev = masik.getFile().getAbsolutePath() + File.separator;
         }
         nev += t.getValueAt(index, 0);
-        if (t.getValueAt(index, 1) == "") {
-            nev += t.getValueAt(index, 1);
+        if (t.getValueAt(index, 1) != "") {
+            nev += "." + t.getValueAt(index, 1);
         }
+        JOptionPane.showMessageDialog(t, nev);
         return new EFile(new File(nev));
     }
 
@@ -789,20 +815,53 @@ public class TGcommanderView extends FrameView implements MouseListener {
 
     @Action
     public void athelyezes() {
-        JOptionPane.showMessageDialog(menuBar, "SZEVASZ BAZZE");
+        masolas();
+        torles();
     }
 
     @Action
     public void ujKonyvtar() {
-        //id
-        String msg = "bal: "+bal.getFile().getAbsolutePath() + "\n"
-                + "jobb: " + jobb.getFile().getAbsolutePath() + "\n"
-                + "focus: " + focus;
-        JOptionPane.showMessageDialog(menuBar, msg);
+        EFile oldal = bal;
+        if (focus) {
+            oldal = jobb;
+        }
+        String ret = JOptionPane.showInputDialog(menuBar, "Új könyvtár itt: "+oldal.getFile().getAbsolutePath(), "");
+        try {
+            new File(oldal.getFile().getAbsolutePath()+File.separator+ret).mkdir();
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(menuBar, "A könyvtár létrehozása nem sikerült!");
+        }
+        refresh();
     }
 
     @Action
     public void torles() {
+        JTable t = listaBal;
+        if (focus) {
+            t = listaJobb;
+        }
+        if (t.getSelectedRowCount() == 0) {
+            JOptionPane.showMessageDialog(t, "Nincs kijelölt file!");
+            return;
+        }
+        int[] rows = t.getSelectedRows();
+        for (int i : rows) {
+
+            EFile source = null;
+            try {
+                source = genEFile(focus,i,true);
+            } catch (Exception ex) {
+                JOptionPane.showMessageDialog(t, "A forrásfile nem létezik!");
+                return;
+            }
+
+            try {
+                source.deleteEntry();
+            } catch (IOException e) {
+                JOptionPane.showMessageDialog(t, e.getMessage());
+            }
+        }
+        refresh();
     }
 
     @Action
